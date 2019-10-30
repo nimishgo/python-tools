8bfcddb3d779fda21f3bcae13f270146b44742e3
nimishgo <nimish.2711@gmail.com>
2019-10-30 16:25:23 +0530

    Added python script for creating fractal tree

---
+++ fractal_tree.py
@@ -0,0 +1,31 @@
+
+#python program for creating fractals 
+#install tkinter for it
+#for debian based distribution 
+# sudo apt-get install python3-tk
+
+
+import turtle
+
+def tree(branchLen,t):
+    if branchLen > 2:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen-15,t)
+        t.left(40)
+        tree(branchLen-15,t)
+        t.right(20)
+        t.backward(branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color("green")
+    tree(75,t)
+    myWin.exitonclick()
+
+main()
